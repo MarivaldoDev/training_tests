8eca7be091795592730f3fbf73d726494320b53e
Marivaldo Pedro <pedromarivaldo10@gmail.com>
2026-01-10 16:45:10 -0300

    pre-commit adicionado

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,11 @@ app_name = "app"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("tasks/category/<int:category_id>/", views.tasks_by_category, name="tasks_by_category"),
+    path(
+        "tasks/category/<int:category_id>/",
+        views.tasks_by_category,
+        name="tasks_by_category",
+    ),
     path("tasks/", views.task_list, name="tasks"),
     path("categories/", views.category_list, name="categories"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Count, Q
+from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Task
-from django.db.models import Count, Q
 
 
 def home(request):
@@ -10,25 +10,18 @@ def home(request):
 
 def tasks_by_category(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
-    tasks = category.tasks.filter(
-        completed=False
-    ).order_by("start_date")
+    tasks = category.tasks.filter(completed=False).order_by("start_date")
     return render(request, "tasks.html", {"tasks": tasks, "category": category})
 
 
 def task_list(request):
-    tasks = Task.objects.filter(
-        completed=False
-    ).order_by("start_date")
+    tasks = Task.objects.filter(completed=False).order_by("start_date")
     return render(request, "tasks.html", {"tasks": tasks})
 
 
 def category_list(request):
-    categories = (
-        Category.objects.annotate(
-            incomplete_count=Count("tasks", filter=Q(tasks__completed=False))
-        )
-        .filter(incomplete_count__gt=0)
-    )
+    categories = Category.objects.annotate(
+        incomplete_count=Count("tasks", filter=Q(tasks__completed=False))
+    ).filter(incomplete_count__gt=0)
 
     return render(request, "categories.html", {"categories": categories})
