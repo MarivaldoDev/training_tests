658182c27e7bc9ff6e10f2682973ec5dbbd9c601
Marivaldo Pedro <pedromarivaldo10@gmail.com>
2026-01-27 09:53:24 -0300

    melhorando porcentagem de cobertura nos testes

--- app/tests/tests_app_views.py
+++ app/tests/tests_app_views.py
@@ -18,9 +18,40 @@ class AppViewsTests(AppTestBase):
         view = resolve(reverse("app:categories"))
         self.assertIs(view.func, views.category_list)
 
+    def test_app_category_view_loads_categories_where_tasks_exist(self):
+        self.make_category(name="Category 1")
+        self.make_category(name="Category 2")
+        self.make_task(category_data={"name": "Category 1"})
+
+        response = self.client.get(reverse("app:categories"))
+        content = response.content.decode("utf-8")
+        response_context_categories = response.context["categories"]
+
+        self.assertIn("Category 1", content)
+        self.assertNotIn("Category 2", content)
+        self.assertEqual(len(response_context_categories), 1)
+
     def test_app_tasks_by_category_view_function_is_correct(self):
         view = resolve(reverse("app:tasks_by_category", kwargs={"category_id": 1}))
         self.assertIs(view.func, views.tasks_by_category)
+
+    def test_app_tasks_by_category_view_category_not_found_returns_404(self):
+        category = self.make_category()
+        response = self.client.get(
+            reverse("app:tasks_by_category", kwargs={"category_id": category.id + 1})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_app_tasks_by_category_view_loads_tasks(self):
+        self.make_task(category_data={"name": "Category 1"})
+        response = self.client.get(
+            reverse("app:tasks_by_category", kwargs={"category_id": 1})
+        )
+        content = response.content.decode("utf-8")
+        response_context_tasks = response.context["tasks"]
+
+        self.assertIn("Task 1", content)
+        self.assertEqual(len(response_context_tasks), 1)
 
     def test_app_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse("app:home"))
@@ -47,6 +78,31 @@ class AppViewsTests(AppTestBase):
         view = resolve(reverse("app:task_detail", kwargs={"task_id": 1}))
         self.assertIs(view.func, views.task_detail)
 
+    def test_app_tasks_detail_view_task_not_found_returns_404(self):
+        task = self.make_task()
+        response = self.client.get(
+            reverse("app:task_detail", kwargs={"task_id": task.id + 1})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_app_tasks_detail_view_loads_task(self):
+        task = self.make_task()
+        response = self.client.get(
+            reverse("app:task_detail", kwargs={"task_id": task.id})
+        )
+        content = response.content.decode("utf-8")
+        response_context_task = response.context["task"]
+
+        self.assertIn("Task 1", content)
+        self.assertEqual(response_context_task, task)
+
     def test_app_toggle_task_completed_view_function_is_correct(self):
         view = resolve(reverse("app:toggle_task_completed", kwargs={"task_id": 1}))
         self.assertIs(view.func, views.toggle_task_completed)
+
+    def test_app_toggle_task_completed_view_task_not_found_returns_404(self):
+        task = self.make_task()
+        response = self.client.get(
+            reverse("app:toggle_task_completed", kwargs={"task_id": task.id + 1})
+        )
+        self.assertEqual(response.status_code, 404)
