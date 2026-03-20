6651d12af4410b3be894672d2032e91ce6cb5e9a
Marivaldo Pedro <pedromarivaldo10@gmail.com>
2026-03-19 21:25:46 -0300

    :up: melhorando herança dos templates e criando mais alguns testes funcionais

--- tests/funcional_tests/authors/test_authors_login_form.py
+++ tests/funcional_tests/authors/test_authors_login_form.py
@@ -1,7 +1,8 @@
-import time
-
+from django.urls import resolve, reverse
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from .base import AuthorsBaseFuncionalTest
 
@@ -17,11 +18,25 @@ class AuthorsLoginFormFunctionalTest(AuthorsBaseFuncionalTest):
         password.send_keys("12345")
         password.send_keys(Keys.ENTER)
 
-        time.sleep(5)
-
-        message_erro = self.browser.find_element(By.CLASS_NAME, "message")
+        message_erro = WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "message"))
+        )
 
         self.assertIn(
             "Por favor, entre com um usuário e senha corretos. Note que ambos os campos diferenciam maiúsculas e minúsculas.",
             message_erro.text,
         )
+
+    def test_load_tasks_if_login_form_is_valid(self):
+        self.browser.get(self.live_server_url + "/authors/login/")
+
+        user = self.browser.find_element(By.ID, "id_username")
+        password = self.browser.find_element(By.ID, "id_password")
+
+        user.send_keys("mariva")
+        password.send_keys("1234")
+        password.send_keys(Keys.ENTER)
+
+        response = resolve(reverse("tasks:tasks", kwargs={"author_id": 1}))
+
+        self.assertEqual(response.url_name, "tasks")
